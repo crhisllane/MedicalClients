795c64c7892658501ee1f3112d8bade2bc512660
Crhisllane <crhisllane@gmail.com>
2019-09-28 08:32:45 -0300

    alteracoes para api no pythonanywhere

--- formulario/formulario/settings.py
+++ formulario/formulario/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '6r@fnw=y%c5jf8uw1duuw48hf+^x4ou+dgfv8lyq(6jq*3512o'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
 
 # Application definition
@@ -123,6 +123,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 
 REST_FRAMEWORK = {
     'DEFAULT_FILTER_BACKENDS': ('django_filters.rest_framework.DjangoFilterBackend',)
